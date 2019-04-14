1231c78c0958880e79d5f3bb73553df14e07b144
DTskhai <trongood@mail.ru>
2019-04-14 22:50:31 +0300

    master

---
+++ Goober.Logging/LoggingConfigurationExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
+using System.Reflection;
+
+namespace Goober.Logging
+{
+    public static class LoggingConfigurationExtensions
+    {
+        public static void UseLoggingVariables(this IApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.UseMiddleware(typeof(LoggingMiddleware));
+        }
+
+        public static void UseExceptionsHandling(this IApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.UseMiddleware(typeof(ErrorHandlingMiddleware));
+        }
+
+        public static IWebHostBuilder UseHermesLogging(this IWebHostBuilder webHostBuilder, string environmentVariable = "ASPNETCORE_ENVIRONMENT")
+        {
+            var nLogConfiguration = NLog.LogManager.Configuration;
+
+            if (nLogConfiguration != null)
+            {
+                nLogConfiguration.Variables["ENVIRONMENT"] = Environment.GetEnvironmentVariable(environmentVariable);
+                nLogConfiguration.Variables["APPLICATION"] = Assembly.GetEntryAssembly().GetName().Name;
+            }
+
+            return webHostBuilder
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                }
+                )
+                .UseNLog();
+        }
+    }
+}
---
+++ Goober.Logging/LoggingMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goober.Logging
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string headers = string.Empty;
+            foreach (var key in context.Request.Headers.Keys)
+                headers += key + "=" + context.Request.Headers[key] + Environment.NewLine;
+
+            context.Items["RESPONSE_CODE"] = context.Response?.StatusCode.ToString();
+            context.Items["USER_IDENTITY"] = context.User?.Identity?.Name;
+            context.Items["REQUEST_HEADERS"] = headers;
+            context.Items["SESSION_ID"] = context.Request?.Headers["MS-ASPNETCORE-TOKEN"].ToString();
+
+            await next(context);
+        }
+
+        private string GetDocumentContents(Stream request)
+        {
+            string documentContents;
+            using (Stream receiveStream = request)
+            {
+
+                using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    documentContents = readStream.ReadToEnd();
+                }
+            }
+            return documentContents;
+        }
+    }
+}
