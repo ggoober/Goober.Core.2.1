4466c2955be519a52ad5c2f7127fd69410896a3c
DTskhai <trongood@mail.ru>
2019-04-27 23:24:02 +0300

    master

---
+++ Goober.WebApp/GooberStartup.cs
@@ -0,0 +1,86 @@
+﻿using Goober.Core.Extensions;
+using Goober.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace Goober.WebApp
+{
+    public abstract class GooberStartup
+    {
+        protected IConfiguration Configuration { get; private set; }
+
+        public GooberStartup(IHostingEnvironment env, IConfiguration config)
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.RegisterDateTimeService();
+            services.AddCaching();
+            services.AddSingleton(Configuration);
+
+            services.AddMvc()
+                    .AddJsonOptions(options =>
+                    {
+                        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                        options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                        options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+                    });
+
+            ConfigureServiceCollections(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory, IApplicationLifetime appLifetime)
+        {
+            app.UseExtenedLoggingVariables();
+
+            UseRequestLocalizationByDefault(app);
+
+            app.UseNlogExceptionsHandling();
+
+            ConfigurePipelineAfterExceptionsHandling(app);
+
+            app.UseStaticFiles();
+
+            app.UseMvc(ConfigureRoutes);
+
+            ConfigurePipelineAfterMvc(app);
+        }
+
+        protected abstract void ConfigureServiceCollections(IServiceCollection services);
+
+        protected abstract void ConfigurePipelineAfterExceptionsHandling(IApplicationBuilder app);
+
+        protected abstract void ConfigurePipelineAfterMvc(IApplicationBuilder app);
+
+        protected abstract void ConfigureRoutes(IRouteBuilder routes);
+
+        private static void UseRequestLocalizationByDefault(IApplicationBuilder app)
+        {
+            var supportedCultures = new[] {
+                new CultureInfo("en"),
+                new CultureInfo("ru"),
+            };
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("ru"),
+                // Formatting numbers, dates, etc.
+                SupportedCultures = supportedCultures,
+                // UI strings that we have localized.
+                SupportedUICultures = supportedCultures
+            });
+        }
+    }
+}
